d8bb5af051fb1416d217ad9f362eba3fac48bf91
jakkh8 <jakhh8@gmail.com>
2024-06-16 19:21:21 +0200

    Add expand to RangeExtension

--- src/range_extension.rs
+++ src/range_extension.rs
@@ -1,19 +1,28 @@
 use std::ops::RangeInclusive;
 
-pub trait RangeExtension {
+pub trait RangeExtension<T> {
     #[allow(unused)]
-    fn surrounds(&self, value: f64) -> bool;
+    fn surrounds(&self, value: T) -> bool;
 
     #[allow(unused)]
-    fn clamp(&self, value: f64) -> f64;
+    fn clamp(&self, value: T) -> T;
+
+    #[allow(unused)]
+    fn expand(&self, delta: T) -> Self;
 }
 
-impl RangeExtension for RangeInclusive<f64> {
+impl RangeExtension<f64> for RangeInclusive<f64> {
     fn surrounds(&self, value: f64) -> bool {
         *self.start() < value && value < *self.end()
     }
 
     fn clamp(&self, value: f64) -> f64 {
         value.max(*self.start()).min(*self.end())
+    }
+
+    fn expand(&self, delta: f64) -> Self {
+        let padding = delta / 2.0;
+
+        self.start() - padding..=self.end() + padding
     }
 }
